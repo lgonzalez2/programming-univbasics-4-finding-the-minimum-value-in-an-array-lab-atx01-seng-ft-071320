acc02fcd56f4a628c01fe5f61b52debd974519c0
lgonzalez2 <lgonzalez_94@hotmail.com>
2020-07-13 18:34:56 -0500

    Done.

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,10 @@
 def find_min_value(array)
-  # Add your solution here
+  min_value = 1000
+
+  array.each do |element|
+    if element < min_value == true 
+      min_value = element 
+    end 
+  end 
+  min_value
 end
